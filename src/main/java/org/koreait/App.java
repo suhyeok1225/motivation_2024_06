05b6ad6a0bad757e208524d5891b99e85471b1b1
suhyeok1225 <chaesh1225@gmail.com>
2024-06-28 10:35:39 +0900

    motivation list 명령어 추가

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -11,27 +11,33 @@ public class App {
 
     public void run() {
         System.out.println(" == motivation 실행 ==");
+        int lastId = 1;
+
 
         while (true) {
-            System.out.print("명령어: ");
+            System.out.print("command)");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                System.out.println("== motivation end ==");
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("다시 입력");
                 continue;
-            }
-            if (cmd.equals("add")) {
+            } else if (cmd.equals("add")) {
                 System.out.print("motivation: ");
                 String motivation = sc.nextLine();
                 System.out.print("source: ");
                 String source = sc.nextLine();
-                System.out.println("~번 motivation이 등록 되었습니다.");
+                System.out.printf("%d번 motivation이 등록 되었습니다.\n", lastId);
+                lastId++;
+            } else if (cmd.equals("list")) {
+                System.out.println("== motivation list ==");
+                System.out.printf("  id   //   motivation   //  source   \n");
+                System.out.println("=".repeat(35));
             }
         }
     }
 }
-}
+
 
