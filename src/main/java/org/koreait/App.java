e6cb57d809e4dca73382db0636d5b61ebd8fc5ae
suhyeok1225 <chaesh1225@gmail.com>
2024-06-28 11:17:59 +0900

    Motivation ion source ê길이 해결

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -36,11 +36,29 @@ public class App {
                 String source = sc.nextLine();
 
                 Motivation motivation = new Motivation(id, body, source);
+                motivations.add(motivation);
                 System.out.printf("%d번 motivation이 등록 되었습니다\n", id);
+                lastId++;
             } else if (cmd.equals("list")) {
+
+                if (motivations.size() == 0) {
+                    System.out.println("등록된 motivation 없음");
+                    continue;
+                }
                 System.out.println("== motivation list ==");
                 System.out.printf("  id   //   motivation   //  source   \n");
                 System.out.println("=".repeat(35));
+                for (int i = motivations.size() - 1; i >= 0; i--) {
+                    Motivation motivation = motivations.get(i);
+                    if (motivation.getSource().length() > 7) {
+                        System.out.printf("   %d  //    %s     //    %s  \n",
+                                motivation.getId(), motivation.getSource().substring(0, 5), motivation.getBody());
+                        continue;
+                    }
+                    System.out.printf("   %d  //    %s     //    %s  \n",
+                            motivation.getId(), motivation.getSource(), motivation.getBody());
+                }
+
             }
         }
     }
