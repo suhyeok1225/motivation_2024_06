06418b050ff51cf00da8f8f372882cabd02ee0c2
suhyeok1225 <chaesh1225@gmail.com>
2024-06-28 13:34:51 +0900

    MotiMotivation Refactoring

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,67 +1,42 @@
 package org.koreait;
 
+import org.koreait.motivation.controller.MotivationController;
+import org.koreait.motivation.entity.Motivation;
+import org.koreait.system.controller.SystemController;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
-
     public App(Scanner sc) {
         this.sc = sc;
     }
 
     public void run() {
-        System.out.println(" == motivation 실행 ==");
-        int lastId = 0;
-        List<Motivation> motivations = new ArrayList<>();
+        System.out.println("== motivation execution ==");
 
+        SystemController systemController = new SystemController();
+        MotivationController motivationController = new MotivationController(sc);
 
         while (true) {
-            System.out.print("command)");
+            System.out.print("command) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation end ==");
+                systemController.exit();
                 break;
             } else if (cmd.length() == 0) {
-                System.out.println("다시 입력");
+                System.out.println("명령어 입력해");
                 continue;
             }
             if (cmd.equals("add")) {
-                int id = lastId + 1;
-                System.out.print("body : ");
-                String body = sc.nextLine();
-                System.out.print("source: ");
-                String source = sc.nextLine();
-
-                Motivation motivation = new Motivation(id, body, source);
-                motivations.add(motivation);
-                System.out.printf("%d번 motivation이 등록 되었습니다\n", id);
-                lastId++;
+                motivationController.add();
             } else if (cmd.equals("list")) {
-
-                if (motivations.size() == 0) {
-                    System.out.println("등록된 motivation 없음");
-                    continue;
-                }
-                System.out.println("== motivation list ==");
-                System.out.printf("  id   //   motivation   //  source   \n");
-                System.out.println("=".repeat(35));
-                for (int i = motivations.size() - 1; i >= 0; i--) {
-                    Motivation motivation = motivations.get(i);
-                    if (motivation.getSource().length() > 7) {
-                        System.out.printf("   %d  //    %s     //    %s  \n",
-                                motivation.getId(), motivation.getSource().substring(0, 5), motivation.getBody());
-                        continue;
-                    }
-                    System.out.printf("   %d  //    %s     //    %s  \n",
-                            motivation.getId(), motivation.getSource(), motivation.getBody());
-                }
-
+                motivationController.list();
             }
         }
     }
 }
-
 
